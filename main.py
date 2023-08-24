0832f792ab6ec7de870ee341109bedeba7c4a9a4
KaZzBerg <139897078+KaZzBerg@users.noreply.github.com>
2023-08-24 21:28:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import re
-
 FILE = open('task9.txt', 'r+')
 
-result = [FILE.readline()]
+file_list = [FILE.readline()]
 
-new_result = result[0].replace(',', ' ').split()
+split_list = file_list[0].split()
+
+for i in split_list:
+    if i == 'a':
+        i = split_list.remove('a')
+    if i == 'an':
+        i = split_list.remove('an')
+    if i == ',':
+        i = split_list.remove(',')
+    else:
+        pass
+
 new_list = []
 
 
 def get_words():
-    for i in new_result:
+    for i in split_list:
         if len(i) >= 7:
             new_list.append(i)
     print(new_list)
@@ -19,5 +28,5 @@ print(get_words())
 with open('Append.txt', 'w') as file:
     print(new_list, file=file)
 
-words = len(new_result)
-print(f'A number of words are: {words}')
+number_words = len(split_list)
+print(f'A number of words are: {number_words}')
